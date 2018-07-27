d06fadb490d82e631baea57efcc37d32bd06efd5
Alex Xie <xiealex@hotmail.com>
2018-07-26 23:39:14 -0700

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	public HelloWorld () { 
+
+	}
+	public static void main (String[] args) {
+		System.out.println("Hello KAT");
+		System.out.println("Hello Alex");
+		System.out.println("why are you doing that");
+	
+	}
+	
+}
